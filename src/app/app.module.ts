687a14e85192f09a1f414b3f4613ceda29d6e229
damour <tuyishimejeand88@gmail.com>
2021-02-26 08:00:32 +0200

    adding  product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PricesPipe } from './core/pipes/prices.pipe';
 import { SharedModule } from './shared/shared.module';
+import { ProductViewComponent } from './components/site/product-view/product-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PricesPipe
+    PricesPipe,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
